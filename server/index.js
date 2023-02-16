9dd51bb838a888f41f88173ad18a0f3c5969806e
Sean DesMarteau <sean.desmarteau@gusto.com>
2023-02-15 19:40:21 -0700

    Update Server Index Path

--- server/index.js
+++ server/index.js
@@ -25,11 +25,11 @@ app.get("/api/passwords", (req, res) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-    // Have Node serve the files for our built React app
-    app.use(express.static(path.join(__dirname, "client/build")));
-
     // All other GET requests not handled before will return our React app
     const path = require("path");
+
+    // Have Node serve the files for our built React app
+    app.use(express.static(path.join(__dirname, "client/build")));
 
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
